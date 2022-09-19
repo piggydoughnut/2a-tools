24178edeb8c77452d3bfd6b34c43fbce87b16205
piggydoughnut <piggydoughnut@gmail.com>
2022-09-19 19:38:40 +0800

    added validation for the form

--- components/InvoicePreview.tsx
+++ components/InvoicePreview.tsx
@@ -1,38 +1,51 @@
 import Iframe from "react-iframe";
+import { format } from "date-fns";
 
 const InvoicePreview = ({
+  projectName,
+  invoiceNumber,
   setPdfUrl,
   pdfUrl,
 }: {
+  projectName: string;
+  invoiceNumber: string;
   setPdfUrl: any;
   pdfUrl: string;
-}) => (
-  <div>
-    <div className="flex flex-row justify-between align-baseline mt-20 ">
-      <button
-        className="font-inriaSans border-0 w-44 h-12  rounded-sm mb-10 bg-gray-100"
-        onClick={() => setPdfUrl(null)}
-      >
-        Edit The Invoice
-      </button>
-      <h3>202212-projectName-12</h3>
-      <button
-        className="font-inriaSans border-0 w-44 h-12 rounded-sm mb-10 bg-green-100"
-        onClick={() => setPdfUrl(null)}
-      >
-        Save to Google Drive
-      </button>
-    </div>
+}) => {
+  const name = projectName
+    .replace(/\b\w/g, (l) => l.toUpperCase())
+    .replaceAll(" ", "-");
+  return (
+    <div>
+      <div className="flex flex-row justify-between align-baseline mt-20 ">
+        <button
+          className="font-inriaSans border-0 w-44 h-12  rounded-sm mb-10 bg-gray-100"
+          onClick={() => setPdfUrl(null)}
+        >
+          Edit The Invoice
+        </button>
+        <h3 className="text-center">
+          This document will be saved as <br />
+          {invoiceNumber.replace("#", "")}-{name}.pdf
+        </h3>
+        <button
+          className="font-inriaSans border-0 w-44 h-12 rounded-sm mb-10 bg-green-100"
+          onClick={() => setPdfUrl(null)}
+        >
+          Save to Google Drive
+        </button>
+      </div>
 
-    <Iframe
-      url={pdfUrl}
-      width="100%"
-      height="800px"
-      id="myId"
-      className="myClassname"
-      position="relative"
-    />
-  </div>
-);
+      <Iframe
+        url={pdfUrl}
+        width="100%"
+        height="800px"
+        id="myId"
+        className="myClassname"
+        position="relative"
+      />
+    </div>
+  );
+};
 
 export default InvoicePreview;
--- pages/invoice.js
+++ pages/invoice.js
@@ -1,4 +1,6 @@
-import { Field, FieldArray, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+import { Field, FieldArray, Form, Formik, yupToFormErrors } from "formik";
 import { getInvoiceNumber, getTotal, processNumber } from "../util/helpers";
 
 import Image from "next/image";
@@ -7,6 +9,25 @@ import axios from "axios";
 import { paymentValues } from "../config";
 import { useState } from "react";
 
+const ItemSchema = Yup.object().shape({
+  item: Yup.string()
+    .required("Required")
+    .max(500, "500 characters max.")
+    .min(1),
+  qty: Yup.number().required("Required").max(1000).min(1, "Minimum 1"),
+  price: Yup.number().required("Required").min(1, "Minimum 1"),
+});
+
+const InvoiceSchema = Yup.object().shape({
+  invoiceNumber: Yup.string().min(0).required("Required"),
+  projectNumber: Yup.number("Must be a number").required("Required"),
+  projectName: Yup.string().required("Required").max(100),
+  jobTitle: Yup.string().max(200),
+  issueDate: Yup.date().required("Required"),
+  dueDate: Yup.date().required("Required"),
+  billto: Yup.string().required("Required").max(300),
+  items: Yup.array().of(ItemSchema).min(1).required("Required"),
+});
 const newValue = {
   item: "",
   qty: 1,
@@ -33,6 +54,22 @@ const getPDF = async (params) => {
     console.log(e);
   }
 };
+
+const Error = ({ children }) => (
+  <p className="text-sm text-red-400">{children}</p>
+);
+
+const showErrors = (errors, touched, fieldName) =>
+  errors[fieldName] && touched[fieldName] ? (
+    <Error>{errors[fieldName]}</Error>
+  ) : null;
+
+const showItemErrors = (touched, errors, idx, fieldName) =>
+  touched.items &&
+  touched.items[idx]?.item &&
+  errors &&
+  errors.items &&
+  errors.items[idx] && <Error>{errors.items[idx][fieldName]}</Error>;
 
 export default function InvoiceGeneratorPage() {
   const [pdfUrl, setPdfUrl] = useState(null);
@@ -71,11 +108,17 @@ export default function InvoiceGeneratorPage() {
       </div>
 
       {pdfUrl ? (
-        <InvoicePreview setPdfUrl={(s) => setPdfUrl(s)} pdfUrl={pdfUrl} />
+        <InvoicePreview
+          setPdfUrl={(s) => setPdfUrl(s)}
+          pdfUrl={pdfUrl}
+          projectName={params.projectName}
+          invoiceNumber={params.invoiceNumberFull}
+        />
       ) : (
         <Formik
           initialValues={params}
           enableReinitialize
+          validationSchema={InvoiceSchema}
           onSubmit={async (vs) => {
             // @todo explore if you can make these fields Hidden form fields in Formik
             vs.amountDue = amountDue;
@@ -90,7 +133,8 @@ export default function InvoiceGeneratorPage() {
             const pdfData = await getPDF(vs);
             setPdfUrl(pdfData);
           }}
-          render={({ values }) => (
+        >
+          {({ values, errors, touched }) => (
             <Form>
               <div className="flex flex-col mt-12">
                 <div className="flex flex-col mb-20">
@@ -99,20 +143,22 @@ export default function InvoiceGeneratorPage() {
                       Project number
                     </label>
                     <Field
-                      className="w-44 bg-yellowy mb-2 border-0 rounded-sm pt-1 pb-1 pl-2 focus:outline-none focus:ring focus:border-yellow-700"
+                      className="w-44 bg-yellowy mb-2 border-0 rounded-sm pt-1 pb-1 pl-2 focus:outline-none focus:ring focus:border-yellow-700 mr-6"
                       name="projectNumber"
                       type="text"
                     />
+                    {showErrors(errors, touched, "projectNumber")}
                   </div>
                   <div className="flex flex-row">
                     <label className="w-36" htmlFor="projectName">
                       Project name
                     </label>
                     <Field
-                      className="w-96 bg-yellowy mb-2 border-0 rounded-sm pt-1 pb-1 pl-2"
+                      className="w-96 bg-yellowy mb-2 border-0 rounded-sm pt-1 pb-1 pl-2 mr-6"
                       name="projectName"
                       type="text"
                     />
+                    {showErrors(errors, touched, "projectName")}
                   </div>
                 </div>
                 <div>
@@ -155,13 +201,17 @@ export default function InvoiceGeneratorPage() {
                             ? values.projectNumber + "-"
                             : null}
                         </p>
-                        <div className="text-lg self-align-baseline pt-8 ml-4 w-20">
+                        <div className="text-lg self-align-baseline pt-8 ml-4 w-42">
                           <Field
                             name="invoiceNumber"
                             type="text"
-                            placeholder="number"
-                            className="w-36"
+                            placeholder="invoice number"
+                            className="w-42"
                           />
+                        </div>
+                        <div className="self-align-baseline mt-10 ml-4">
+                          {console.log(errors)}
+                          {showErrors(errors, touched, "invoiceNumber")}
                         </div>
                       </div>
                     </div>
@@ -195,7 +245,7 @@ export default function InvoiceGeneratorPage() {
                             className="grid gap-3 grid-cols-8 mb-4"
                           >
                             <Field
-                              className="bg-yellowy border-0 rounded-sm pt-1 pb-1 pl-2 col-span-4 "
+                              className="bg-yellowy border-0 rounded-sm pt-1 pb-1 pl-2 col-span-4"
                               name={`items[${idx}].item`}
                               type="text"
                               value={val.item}
@@ -204,8 +254,8 @@ export default function InvoiceGeneratorPage() {
                             <Field
                               className="bg-yellowy border-0 rounded-sm pt-1 pb-1 pl-2 w-20 justify-self-end"
                               name={`items[${idx}].price`}
-                              pattern="[0-9]*.?[0-9]*"
-                              type="text"
+                              // pattern="[0-9]*.?[0-9]*"
+                              type="number"
                               value={val.price}
                             />
                             <Field
@@ -225,6 +275,15 @@ export default function InvoiceGeneratorPage() {
                                 Remove
                               </div>
                             )}
+                            <div className="col-span-4">
+                              {showItemErrors(touched, errors, idx, "item")}
+                            </div>
+                            <div className="col-span-1 justify-self-end">
+                              {showItemErrors(touched, errors, idx, "price")}
+                            </div>
+                            <div className="col-span-1 justify-self-end">
+                              {showItemErrors(touched, errors, idx, "qty")}
+                            </div>
                           </div>
                         ))}
                         <button
@@ -263,7 +322,7 @@ export default function InvoiceGeneratorPage() {
               </div>
             </Form>
           )}
-        ></Formik>
+        </Formik>
       )}
       <div className="flex flex-row justify-center"></div>
     </div>
