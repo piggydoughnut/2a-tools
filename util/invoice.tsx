e0d3f465eeb9f6a0f256cdd310589fc5b984df73
piggydoughnut <piggydoughnut@gmail.com>
2022-09-23 11:36:38 +0800

    added configuration for pdf in a separate file

--- util/invoice.tsx
+++ util/invoice.tsx
@@ -1,36 +1,15 @@
+import {
+  Colors,
+  FontSize,
+  Fonts,
+  LOGO_IMAGE,
+  PageParams,
+} from "./pdfStyleConfig";
 import { format, parseISO } from "date-fns";
-import { readFileSync, write } from "fs";
 
 import PDFDocument from "pdfkit-table";
 import { contact } from "config";
 import { pEvent } from "p-event";
-import path from "path";
-
-const MARGIN = 40;
-// const A4_HEIGHT = 841.89;
-const A4_WIDTH = 595.28;
-
-const BLACK = "#000000";
-const GRAY = "#6F6E70";
-const LINE_HEIGHT = 20;
-
-const FONT_TEXT = 10;
-const FONT_H1 = 40;
-const FONT_H2 = 18;
-const FONT_H3 = 16;
-
-const mainPath = path.join(process.cwd(), "public");
-const fontsPath = path.join(mainPath, "fonts");
-const INRIA_BOLD = path.join(fontsPath, "InriaSans-Bold.ttf");
-const ROBOTO_MONO = path.join(fontsPath, "RobotoMono-VariableFont_wght.ttf");
-const INRIA_REGULAR = path.join(fontsPath, "InriaSans-Regular.ttf");
-const logoImage = path.join(process.cwd(), "/public", "logo.png");
-
-const font1 = readFileSync(INRIA_BOLD);
-const font2 = readFileSync(ROBOTO_MONO);
-const font3 = readFileSync(INRIA_REGULAR);
-
-const drawLine = () => {};
 
 export type InvoiceProps = {
   invoiceNumberFull: number;
@@ -69,10 +48,10 @@ export const generatePdf = async ({
       bufferPages: true,
       size: "A4",
       margins: {
-        top: MARGIN,
-        bottom: MARGIN,
-        left: MARGIN,
-        right: MARGIN,
+        top: PageParams.MARGIN,
+        bottom: PageParams.MARGIN,
+        left: PageParams.MARGIN,
+        right: PageParams.MARGIN,
       },
     });
 
@@ -82,66 +61,75 @@ export const generatePdf = async ({
       bufferChunks.push(doc.read());
     });
 
-    doc.opacity(0.05).rect(0, 0, A4_WIDTH, 240).fill(BLACK);
-    doc.fill(BLACK).opacity(1);
+    doc.opacity(0.05).rect(0, 0, PageParams.A4_WIDTH, 240).fill(Colors.BLACK);
+    doc.fill(Colors.BLACK).opacity(1);
 
-    doc.image(logoImage, MARGIN, MARGIN, {
+    doc.image(LOGO_IMAGE, PageParams.MARGIN, PageParams.MARGIN, {
       width: 150,
     });
 
-    const writeBold = (fontSize: number) => {
-      try {
-        doc.fontSize(fontSize);
-        doc.font(font1);
-      } catch (e) {
-        console.log(e);
+    const writeBold = (fontSize?: number | null) => {
+      if (!fontSize) {
+        fontSize = FontSize.P;
       }
+      doc.fontSize(fontSize);
+      doc.font(Fonts.inriaBold);
     };
 
-    const writeNumbers = (fontSize: number) => {
+    const writeNumbers = (fontSize?: number | null) => {
+      if (!fontSize) {
+        fontSize = FontSize.P;
+      }
       doc.fontSize(fontSize);
-      doc.font(font2);
+      doc.font(Fonts.robotoMono);
     };
 
-    const writeText = (fontSize: number) => {
+    const writeText = (fontSize?: number | null) => {
+      if (!fontSize) {
+        fontSize = FontSize.P;
+      }
       doc.fontSize(fontSize);
-      doc.font(font3);
+      doc.font(Fonts.inriaRegular);
     };
 
     const HEADER_X = 400;
 
-    writeBold(FONT_TEXT);
-    doc.text("DUE DATE", HEADER_X, MARGIN);
-    writeText(FONT_TEXT);
+    writeBold();
+    doc.text("DUE DATE", HEADER_X, PageParams.MARGIN);
+    writeText();
     doc.text(
       format(parseISO(dueDate.toString()), "yyyy-MM-dd"),
       HEADER_X,
-      MARGIN + LINE_HEIGHT * 0.75
+      PageParams.MARGIN + PageParams.LINE_HEIGHT * 0.75
     );
 
-    writeBold(FONT_TEXT);
-    doc.text("ISSUE DATE", HEADER_X, MARGIN + LINE_HEIGHT * 2);
-    writeText(FONT_TEXT);
+    writeBold();
+    doc.text(
+      "ISSUE DATE",
+      HEADER_X,
+      PageParams.MARGIN + PageParams.LINE_HEIGHT * 2
+    );
+    writeText();
     doc.text(
       format(parseISO(issueDate.toString()), "yyyy-MM-dd"),
       HEADER_X,
-      MARGIN + LINE_HEIGHT * 2.75
+      PageParams.MARGIN + PageParams.LINE_HEIGHT * 2.75
     );
 
-    doc.text(billto, HEADER_X + 70, MARGIN, {
+    doc.text(billto, HEADER_X + 70, PageParams.MARGIN, {
       align: "right",
     });
 
-    writeBold(FONT_H1);
-    doc.text("INVOICE", MARGIN, 180, {
+    writeBold(FontSize.H1);
+    doc.text("INVOICE", PageParams.MARGIN, 180, {
       align: "left",
       characterSpacing: 8,
     });
 
-    writeNumbers(FONT_H2);
-    doc.text(`${invoiceNumberFull}`, MARGIN + 200, 200);
+    writeNumbers(FontSize.H2);
+    doc.text(`${invoiceNumberFull}`, PageParams.MARGIN + 200, 200);
 
-    writeText(FONT_TEXT);
+    writeText();
 
     const HEADER_LINE_Y = 250;
 
@@ -254,29 +242,25 @@ export const generatePdf = async ({
       rows: tableData,
     };
 
-    doc.text("", MARGIN, HEADER_LINE_Y);
+    doc.text("", PageParams.MARGIN, HEADER_LINE_Y);
     await doc.table(table, {
       columnsSize: [270, 90, 50, 80],
       prepareHeader: () => {
-        doc.font(font1);
+        writeText();
         return doc;
       },
       prepareRow: (row, indexColumn, indexRow, rectRow, rectCell) => {
-        doc.font(font3);
+        //regular text
+        writeText();
         // numbers
         if (indexColumn != 0) {
-          doc.font(font2);
+          writeNumbers();
         }
         // invoice subtotal block
         if (indexRow === table.rows.length - 1) {
-          writeBold(FONT_H3);
+          writeBold(FontSize.H3);
         }
         if (indexRow === table.rows.length - 4 && indexColumn !== 0) {
-          if (indexColumn === 1) {
-            doc.font(font3);
-          } else {
-            doc.font(font2);
-          }
           let offset = 0;
           if (indexColumn === 1) {
             offset = 32;
@@ -296,41 +280,52 @@ export const generatePdf = async ({
     });
 
     ////////// FOOTER /////////////
-    const FOOTER_HEIGHT = 680;
-    writeBold(FONT_H2);
-    doc.text("Thank you", MARGIN, FOOTER_HEIGHT);
+    writeBold(FontSize.H2);
+    doc.text("Thank you", PageParams.MARGIN, PageParams.FOOTER_HEIGHT);
     doc
       .lineCap("butt")
-      .moveTo(MARGIN, FOOTER_HEIGHT + 30)
-      .lineTo(555, FOOTER_HEIGHT + 30)
+      .moveTo(PageParams.MARGIN, PageParams.FOOTER_HEIGHT + 30)
+      .lineTo(555, PageParams.FOOTER_HEIGHT + 30)
       .stroke();
 
-    writeBold(FONT_TEXT);
+    writeBold();
     const COEF = 0.7;
 
     doc
-      .fillColor(GRAY)
-      .text("PAYABLE TO", MARGIN, FOOTER_HEIGHT + LINE_HEIGHT * 2);
-    writeText(FONT_TEXT);
-    doc.fillColor(BLACK);
+      .fillColor(Colors.GRAY)
+      .text(
+        "PAYABLE TO",
+        PageParams.MARGIN,
+        PageParams.FOOTER_HEIGHT + PageParams.LINE_HEIGHT * 2
+      );
+    writeText();
+    doc.fillColor(Colors.BLACK);
     paymentValues.map((value, index) => {
-      let height = FOOTER_HEIGHT + LINE_HEIGHT * (index * COEF + 3);
-      doc.text(value.label, MARGIN, height);
+      let height =
+        PageParams.FOOTER_HEIGHT + PageParams.LINE_HEIGHT * (index * COEF + 3);
+      doc.text(value.label, PageParams.MARGIN, height);
       if (value.number) {
-        writeNumbers(FONT_TEXT);
+        writeNumbers();
       } else {
-        writeText(FONT_TEXT);
+        writeText();
       }
-      doc.text(value.value, MARGIN * 4, height);
+      doc.text(value.value, PageParams.MARGIN * 4, height);
     });
 
-    writeBold(FONT_TEXT);
+    writeBold();
 
-    doc.fillColor(GRAY).text("CONTACT", 430, FOOTER_HEIGHT + LINE_HEIGHT * 2);
-    writeText(FONT_TEXT);
-    doc.fillColor(BLACK);
+    doc
+      .fillColor(Colors.GRAY)
+      .text(
+        "CONTACT",
+        430,
+        PageParams.FOOTER_HEIGHT + PageParams.LINE_HEIGHT * 2
+      );
+    writeText();
+    doc.fillColor(Colors.BLACK);
     contact.map((val, index) => {
-      let height = FOOTER_HEIGHT + LINE_HEIGHT * (index * COEF + 3);
+      let height =
+        PageParams.FOOTER_HEIGHT + PageParams.LINE_HEIGHT * (index * COEF + 3);
       doc.text(val, 430, height);
     });
 
---
+++ util/pdfStyleConfig.js
@@ -0,0 +1,35 @@
+import path from "path";
+import { readFileSync } from "fs";
+
+export const PageParams = {
+  MARGIN: 40,
+  A4_WIDTH: 595.28,
+  A4_HEIGHT: 841.89,
+  LINE_HEIGHT: 20,
+  FOOTER_HEIGHT: 680,
+};
+
+export const Colors = {
+  BLACK: "#000000",
+  GRAY: "#6F6E70",
+};
+
+export const FontSize = {
+  P: 10,
+  H1: 40,
+  H2: 18,
+  H3: 16,
+};
+
+const mainPath = path.join(process.cwd(), "public");
+const fontsPath = path.join(mainPath, "fonts");
+
+export const Fonts = {
+  inriaBold: readFileSync(path.join(fontsPath, "InriaSans-Bold.ttf")),
+  robotoMono: readFileSync(
+    path.join(fontsPath, "RobotoMono-VariableFont_wght.ttf")
+  ),
+  inriaRegular: readFileSync(path.join(fontsPath, "InriaSans-Regular.ttf")),
+};
+
+export const LOGO_IMAGE = path.join(mainPath, "logo.png");
