b8ec3f22c62a8aafcb401b0a5e240322633580b7
piggydoughnut <piggydoughnut@gmail.com>
2022-09-23 17:52:59 +0800

    catching the err

--- pages/api/generatePdf.js
+++ pages/api/generatePdf.js
@@ -2,15 +2,19 @@
 import { generatePdf } from "../../util/invoice";
 
 export default async function handler(req, res) {
-  if (req.method === "POST") {
-    try {
-      const pdfData = await generatePdf(req.body);
-      return res.end(pdfData);
-    } catch (e) {
-      console.log(e);
-      return res.send({ err: e.message, stack: e.stack });
+  try {
+    if (req.method === "POST") {
+      try {
+        const pdfData = await generatePdf(req.body);
+        return res.end(pdfData);
+      } catch (e) {
+        console.log(e);
+        return res.send({ err: e.message, stack: e.stack });
+      }
+    } else {
+      return res.json({ what: "yo" });
     }
-  } else {
-    return res.json({ what: "yo" });
+  } catch (e) {
+    return res.json({ err: e.message, stack: e.stack });
   }
 }
