bbb3bc27fe622ad0764b2f3ea9ae5be15c75a541
piggydoughnut <piggydoughnut@gmail.com>
2022-10-09 14:27:47 +0800

    wording change

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,32 @@
 import * as c from "../config";
 
-import Footer from "../components/Footer";
 import Image from "next/image";
 import logo from "../public/logov2-1.svg";
 
 export default function Home() {
   return (
-    <div
-      id="content"
-      className="flex flex-col justify-center font-inriaSans text-midnight-black mx-4 xl:mx-40 mt-10 mb-10"
-    >
-      <Image
-        alt="2aLogo"
-        src={logo}
-        onClick={() => router.push("/")}
-        width="200px"
-        className="hover:cursor-pointer"
-      />
-
-      {/* <h1 className="mx-auto mt-10 text-md">Coming soon...</h1> */}
+    <div className="font-inriaSans flex flex-col justify-center text-midnight-black mx-4 xl:mx-40 mt-10 mb-10">
+      <div className="mx-auto">
+        <Image
+          alt="2aLogo"
+          src={logo}
+          onClick={() => router.push("/")}
+          width="300"
+        />
+      </div>
       <div className="mx-auto">
         <h1 className="text-md mt-10 mb-4">
-          Website is coming soon <br /> For now please contact us at
+          Website is coming soon... <br /> For now please contact us at
         </h1>
-        {c.contact.map((val) => (
-          <p className="text-sm" key={val}>
-            {val}
-          </p>
-        ))}
+        {c.contact.map(
+          (val, idx) =>
+            idx !== 0 && (
+              <p className="text-sm" key={val}>
+                {val}
+              </p>
+            )
+        )}
       </div>
-      {/* <Footer /> */}
     </div>
   );
 }
